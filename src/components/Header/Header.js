21cc37e39848f45dfed2ea4699c5675f9cee532f
= <tunay.ozdemir@hotmail.com>
2021-09-19 02:46:29 +0300

    test3

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import './Header.scss'
+import Logo from '../Logo/Logo'
+import Button from '../Button/Button'
+
+
+
+
+function Header() {
+  const [isActive, setisActive] = useState("")
+
+  const items = [
+    ["Games"],
+    ["Membership"],
+    ["Download"],
+    ["Blog"],
+    ["Support"]
+  ]
+
+useEffect(() => {
+
+}, [])
+
+  return (
+    <header className="header">
+      <Logo />
+      <div className="header__right">
+        <nav className="header__right--nav">{items.map((item, key) => {
+          if (isActive === "active") {
+            {/* debugger */}
+            return (
+              <a href="javascript:;" className="" key={key} title={item}>{item}</a>
+            )
+          }
+          return (
+            <a href="javascript:;" key={key} title={item}>{item}</a>
+          )
+        })
+        }
+        </nav>
+        <Button title="LET'S PLAY" />
+      </div>
+
+
+    </header>
+  )
+}
+export default Header
