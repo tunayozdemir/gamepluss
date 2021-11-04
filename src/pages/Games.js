e69f219677d4170f1a20fed38127f948c68ebefa
Tunay Özdemir <tunay.ozdemir@consultant.turkcell.com.tr>
2021-11-04 12:06:10 +0300

    Navbar deleted

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Accordion from '../components/Accordion/Accordion'
-import Navbar from '../components/Navbar/Navbar'
-import CheckBox from '../components/CheckBox/CheckBox'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Main from '../components/Main/Main'
